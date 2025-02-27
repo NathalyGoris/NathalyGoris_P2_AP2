bb360d7631d6b314422d82303b693585126951af
natha <nathalygorisespino@gmail.com>
2025-02-26 20:34:15 -0400

    Hecho en clases

--- app/src/main/java/ucne/edu/nathalygoris_p2_ap2/data/remote/dto/DepositosDto.kt
+++ app/src/main/java/ucne/edu/nathalygoris_p2_ap2/data/remote/dto/DepositosDto.kt
@@ -2,10 +2,10 @@ package ucne.edu.nathalygoris_p2_ap2.data.remote.dto
 
 import java.time.LocalDate
 
-data class DepositosDto (
+data class HDepositosDto (
     val idDeposito: Int?,
     val fecha: LocalDate? = null,
     val idCuenta: Int? = null,
     val concepto: String? = null,
-    val monto: Double?? = null
+    val monto: Double? = null
 )
